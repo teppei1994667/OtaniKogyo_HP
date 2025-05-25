a78a9aea25b54468b0255e84b4ffeb75101ccbf9
笹田哲平 <teppei@sasadateppeinoMacBook-Pro.local>
2025-05-25 21:20:47 +0900

    メインイメージ部分をサンプルイメージで作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className="relative">
+        <Image
+          src="/メインイメージサンプル.webp"
+          alt="会社ロゴ"
+          width={728}
+          height={484}
+          layout="responsive"
+        />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <h1 className="text-white text-2xl md:text-4xl font-black p-4`">
+            I Make Beautiful Feature
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 }
