219ce69eb796fd861a18a1a400e1c4c9930e97d5
笹田哲平 <teppei@sasadateppeinoMacBook-Pro.local>
2025-06-19 22:58:24 +0900

    CompanyProfileDetail: レスポンシブ対応

--- src/app/CompanyProfile/component/Access.tsx
+++ src/app/CompanyProfile/component/Access.tsx
@@ -4,7 +4,7 @@ export const Access = () => {
   return (
     <>
       <Grid container className="justify-center mt-8">
-        <Grid className="w-3/4 h-120">
+        <Grid className="w-5/6 sm:w-3/4 h-120">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.32704151164!2d135.48648607725372!3d34.64644277293894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e77f3da17b79%3A0x88b31994a7eb7d2b!2z44CSNTU3LTAwMzEg5aSn6Ziq5bqc5aSn6Ziq5biC6KW_5oiQ5Yy66ba06KaL5qmL77yT5LiB55uu77yR4oiS77yS77yU!5e0!3m2!1sja!2sjp!4v1749825074732!5m2!1sja!2sjp"
             width="100%"
--- src/app/CompanyProfile/component/CompanyProfileDetail.tsx
+++ src/app/CompanyProfile/component/CompanyProfileDetail.tsx
@@ -4,69 +4,69 @@ export const CompanyProfileDetail = () => {
   return (
     <>
       <Grid container className="justify-center mt-10">
-        <Grid className="flex items-center bg-gray-400 h-12 w-48 justify-center">
+        <Grid className="flex items-center bg-gray-400 h-12 w-5/6 sm:w-48 sm:justify-center pl-2 sm:pl-0">
           <p className="font-medium text-lg">会社名</p>
         </Grid>
-        <Grid className="flex items-center bg-gray-200 h-12 w-144 ml-4 pl-2">
+        <Grid className="flex items-center bg-gray-200 h-12 w-5/6 sm:w-3/5 lg:w-144 sm:ml-4 pl-2">
           <p className="text-lg">株式会社大谷工業</p>
         </Grid>
       </Grid>
       <Grid container className="justify-center mt-2">
-        <Grid className="flex items-center bg-gray-400 h-24 w-48 justify-center">
+        <Grid className="flex items-center bg-gray-400 h-12 sm:h-24 w-5/6 sm:w-48 sm:justify-center pl-2 sm:pl-0">
           <p className="font-medium text-lg">所在地</p>
         </Grid>
-        <Grid className="flex flex-col justify-center bg-gray-200 h-24 w-144 ml-4 pl-2">
+        <Grid className="flex flex-col sm:justify-center bg-gray-200 h-32 sm:h-24 w-5/6 sm:w-3/5 lg:w-144 sm:ml-4 pt-4 sm:pt-0 pl-2">
           <p className="text-lg">【本社】</p>
-          <p className="text-lg">
-            〒557-0031 大阪府大阪市西成区鶴見橋３丁目１−２４
+          <p className="text-lg whitespace-pre-line md:whitespace-normal">
+            {`〒557-0031\n大阪府大阪市西成区鶴見橋３丁目１−２４`}
           </p>
         </Grid>
       </Grid>
       <Grid container className="justify-center mt-2">
-        <Grid className="flex items-center bg-gray-400 h-12 w-48 justify-center">
-          <p className="text-lg">Tel</p>
+        <Grid className="flex items-center bg-gray-400 h-12 w-5/6 sm:w-48 sm:justify-center pl-2 sm:pl-0">
+          <p className="text-lg">TEL</p>
         </Grid>
-        <Grid className="flex items-center bg-gray-200 h-12 w-144 ml-4 pl-2">
+        <Grid className="flex items-center bg-gray-200 h-12 w-5/6 sm:w-3/5 lg:w-144 sm:ml-4 pl-2">
           <p className="text-lg">06-7654-9938</p>
         </Grid>
       </Grid>
       <Grid container className="justify-center mt-2">
-        <Grid className="flex items-center bg-gray-400 h-12 w-48 justify-center">
+        <Grid className="flex items-center bg-gray-400 h-12 w-5/6 sm:w-48 sm:justify-center pl-2 sm:pl-0">
           <p className="text-lg">資本金</p>
         </Grid>
-        <Grid className="flex items-center bg-gray-200 h-12 w-144 ml-4 pl-2">
+        <Grid className="flex items-center bg-gray-200 h-12 w-5/6 sm:w-3/5 lg:w-144 sm:ml-4 pl-2">
           <p className="text-lg">200万円</p>
         </Grid>
       </Grid>
       <Grid container className="justify-center mt-2">
-        <Grid className="flex items-center bg-gray-400 h-12 w-48 justify-center">
+        <Grid className="flex items-center bg-gray-400 h-12 w-5/6 sm:w-48 sm:justify-center pl-2 sm:pl-0">
           <p className="text-lg">代表</p>
         </Grid>
-        <Grid className="flex items-center bg-gray-200 h-12 w-144 ml-4 pl-2">
+        <Grid className="flex items-center bg-gray-200 h-12 w-5/6 sm:w-3/5 lg:w-144 sm:ml-4 pl-2">
           <p className="text-lg">代表取締役 谷 大介</p>
         </Grid>
       </Grid>
       <Grid container className="justify-center mt-2">
-        <Grid className="flex items-center bg-gray-400 h-12 w-48 justify-center">
+        <Grid className="flex items-center bg-gray-400 h-12 w-5/6 sm:w-48 sm:justify-center pl-2 sm:pl-0">
           <p className="text-lg">創業</p>
         </Grid>
-        <Grid className="flex items-center bg-gray-200 h-12 w-144 ml-4 pl-2">
+        <Grid className="flex items-center bg-gray-200 h-12 w-5/6 sm:w-3/5 lg:w-144 sm:ml-4 pl-2">
           <p className="text-lg">2022年10月31日</p>
         </Grid>
       </Grid>
       <Grid container className="justify-center mt-2">
-        <Grid className="flex items-center bg-gray-400 h-12 w-48 justify-center">
+        <Grid className="flex items-center bg-gray-400 h-12 w-5/6 sm:w-48 sm:justify-center pl-2 sm:pl-0">
           <p className="text-lg">建築業許可番号</p>
         </Grid>
-        <Grid className="flex items-center bg-gray-200 h-12 w-144 ml-4 pl-2">
+        <Grid className="flex items-center bg-gray-200 h-12 w-5/6 sm:w-3/5 lg:w-144 sm:ml-4 pl-2">
           <p className="text-lg">大阪府知事 許可(般 - 5)第161252号</p>
         </Grid>
       </Grid>
       <Grid container className="justify-center mt-2">
-        <Grid className="flex items-center bg-gray-400 h-36 w-48 justify-center">
+        <Grid className="flex items-center bg-gray-400 h-12 sm:h-36 w-5/6 sm:w-48 sm:justify-center pl-2 sm:pl-0">
           <p className="text-lg">業務内容</p>
         </Grid>
-        <Grid className="flex flex-col justify-center bg-gray-200 h-36 w-144 ml-4 pl-2">
+        <Grid className="flex flex-col justify-center bg-gray-200 h-32 sm:h-36 w-5/6 sm:w-3/5 lg:w-144 sm:ml-4 pl-2">
           <p className="text-lg">アスベスト除去工事</p>
           <p className="text-lg">ダイオキシン類対策工事</p>
           <p className="text-lg">解体・仮設足場・外構工事</p>
