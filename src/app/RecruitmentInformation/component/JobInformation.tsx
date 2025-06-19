580f285a8646c6a300c46c8f21cae8a280b8e09b
笹田哲平 <teppei@sasadateppeinoMacBook-Pro.local>
2025-06-19 21:35:45 +0900

    RecruitmentInformation: レスポンシブ対応

--- src/app/RecruitmentInformation/component/JobInformation.tsx
+++ src/app/RecruitmentInformation/component/JobInformation.tsx
@@ -8,23 +8,23 @@ export const JobInformation = () => {
       <Grid container className="mt-16 justify-center">
         <Grid className="text-center w-full">
           <h1 className="font-bold text-3xl">求人情報</h1>
-          <div className="mt-6 mx-auto w-2/3 border-b border-gray-400"></div>
+          <div className="mt-6 mx-auto w-2/3 border-b border-gray-400 hidden sm:block"></div>
         </Grid>
       </Grid>
-      <Grid container className="mt-4 justify-center">
-        <Grid className="text-center w-2/5">
-          <p className="text-base">職種</p>
+      <Grid container className="mt-2 justify-center flex flex-col sm:flex-row">
+        <Grid className="sm:text-center w-9/12 sm:w-2/5">
+          <p className="text-base font-bold">職種</p>
         </Grid>
-        <Grid className="w-2/5">
+        <Grid className="w-8/12 sm:w-2/5 mt-4 sm:mt-0">
           <p className="text-base">現場管理者候補 / 作業員</p>
         </Grid>
       </Grid>
       <BaseBorder />
-      <Grid container className="mt-4 justify-center">
-        <Grid className="text-center w-2/5">
-          <p className="text-base">勤務時間・曜日</p>
+      <Grid container className="mt-2 justify-center flex flex-col sm:flex-row">
+        <Grid className="sm:text-center w-9/12 sm:w-2/5">
+          <p className="text-base font-bold">勤務時間・曜日</p>
         </Grid>
-        <Grid className="w-2/5">
+        <Grid className="w-8/12 sm:w-2/5 mt-4 sm:mt-0">
           <p className="text-base">8：00～17：00</p>
           <p className="text-base mt-4">・基本的に残業なし</p>
           <p className="text-base">・こまめな休憩、水分補給時間あり</p>
@@ -32,20 +32,20 @@ export const JobInformation = () => {
         </Grid>
       </Grid>
       <BaseBorder />
-      <Grid container className="mt-4 justify-center">
-        <Grid className="text-center w-2/5">
-          <p className="text-base">勤務形態</p>
+      <Grid container className="mt-2 justify-center flex flex-col sm:flex-row">
+        <Grid className="sm:text-center w-9/12 sm:w-2/5">
+          <p className="text-base font-bold">勤務形態</p>
         </Grid>
-        <Grid className="w-2/5">
+        <Grid className="w-8/12 sm:w-2/5 mt-4 sm:mt-0">
           <p className="text-base">固定時間制</p>
         </Grid>
       </Grid>
       <BaseBorder />
-      <Grid container className="mt-4 justify-center">
-        <Grid className="text-center w-2/5">
-          <p className="text-base">休暇・休日</p>
+      <Grid container className="mt-2 justify-center flex flex-col sm:flex-row">
+        <Grid className="sm:text-center w-9/12 sm:w-2/5">
+          <p className="text-base font-bold">休暇・休日</p>
         </Grid>
-        <Grid className="w-2/5">
+        <Grid className="w-8/12 sm:w-2/5 mt-4 sm:mt-0">
           <p className="text-base">土曜日、日曜日、祝日</p>
           <p className="text-base mt-4">・土日祝休推奨</p>
           <p className="text-base">・GW休暇</p>
@@ -54,64 +54,70 @@ export const JobInformation = () => {
         </Grid>
       </Grid>
       <BaseBorder />
-      <Grid container className="mt-4 justify-center">
-        <Grid className="text-center w-2/5">
-          <p className="text-base">勤務地所在地</p>
+      <Grid container className="mt-2 justify-center flex flex-col sm:flex-row">
+        <Grid className="sm:text-center w-9/12 sm:w-2/5">
+          <p className="text-base font-bold">勤務地所在地</p>
         </Grid>
-        <Grid className="w-2/5">
+        <Grid className="w-8/12 sm:w-2/5 mt-4 sm:mt-0">
           <p className="text-base">大阪府大阪市住吉区沢之町1-1-13</p>
-          <p className="text-base mt-4">
-            ※現場は天王寺、阿倍野、平尾、生野、堺など多数あります。
+          <p className="text-base mt-4 whitespace-pre-line sm:whitespace-normal">
+            {`※現場は天王寺、阿倍野、平尾、\n生野、堺など多数あります。`}
           </p>
         </Grid>
       </Grid>
       <BaseBorder />
-      <Grid container className="mt-4 justify-center">
-        <Grid className="text-center w-2/5">
-          <p className="text-base">アクセス</p>
+      <Grid container className="mt-2 justify-center flex flex-col sm:flex-row">
+        <Grid className="sm:text-center w-9/12 sm:w-2/5">
+          <p className="text-base font-bold">アクセス</p>
         </Grid>
-        <Grid className="w-2/5">
+        <Grid className="w-8/12 sm:w-2/5 mt-4 sm:mt-0">
           <p className="text-base">XXXXXXXXXXXXX</p>
         </Grid>
       </Grid>
       <BaseBorder />
-      <Grid container className="mt-4 justify-center">
-        <Grid className="text-center w-2/5">
-          <p className="text-base">給与</p>
+      <Grid container className="mt-2 justify-center flex flex-col sm:flex-row">
+        <Grid className="sm:text-center w-9/12 sm:w-2/5">
+          <p className="text-base font-bold">給与</p>
         </Grid>
-        <Grid className="w-2/5">
+        <Grid className="w-8/12 sm:w-2/5 mt-4 sm:mt-0">
           <p className="text-base">XXXXXXXXXXXXX</p>
         </Grid>
       </Grid>
       <BaseBorder />
-      <Grid container className="mt-4 justify-center">
-        <Grid className="text-center w-2/5">
-          <p className="text-base">試用期間</p>
+      <Grid container className="mt-2 justify-center flex flex-col sm:flex-row">
+        <Grid className="sm:text-center w-9/12 sm:w-2/5">
+          <p className="text-base font-bold">試用期間</p>
         </Grid>
-        <Grid className="w-2/5">
+        <Grid className="w-8/12 sm:w-2/5 mt-4 sm:mt-0">
           <p className="text-base">試用期間あり</p>
           <p className="text-base">試用期間：3か月</p>
           <p className="text-base">試用期間中の労働条件：同条件</p>
         </Grid>
       </Grid>
       <BaseBorder />
-      <Grid container className="mt-4 justify-center">
-        <Grid className="text-center w-2/5">
-          <p className="text-base">待遇・福利厚生</p>
+      <Grid container className="mt-2 justify-center flex flex-col sm:flex-row">
+        <Grid className="sm:text-center w-9/12 sm:w-2/5">
+          <p className="text-base font-bold">待遇・福利厚生</p>
         </Grid>
-        <Grid className="w-2/5">
+        <Grid className="w-8/12 sm:w-2/5 mt-4 sm:mt-0">
           <p className="text-base">XXXXXXXXXXXXX</p>
-          <p className="text-base mt-4">XXXXXXXXXXXXX</p>
-          <p className="text-base mt-4">XXXXXXXXXXXXX</p>
+          <p className="text-base">XXXXXXXXXXXXX</p>
+          <p className="text-base">XXXXXXXXXXXXX</p>
         </Grid>
       </Grid>
-      <Grid container className="mt-12 justify-center">
+      <Grid
+        container
+        className="mt-10 justify-center flex flex-col sm:flex-row"
+      >
         <Grid className="flex items-center">
-          <p className="text-lg font-bold">
-            お問い合わせ・応募については右記よりご連絡ください。
+          <p className="text-lg font-bold whitespace-pre-line sm:whitespace-normal text-center hidden sm:block">
+            {`お問い合わせ・応募については\n右記よりご連絡ください。`}
+          </p>
+          <p className="text-lg font-bold whitespace-pre-line sm:whitespace-normal text-center block sm:hidden">
+            {`お問い合わせ・応募については\n下記よりご連絡ください。`}
           </p>
         </Grid>
-        <Grid className="ml-24">
+        <Grid className="sm:ml-24 mt-4 sm:mt-0">
           <button className="text-xs lg:text-base bg-white border border-black p-3 md:p-5 lg:p-6 hover:bg-gray-200 transition-colors duration-300">
             {`お問い合わせフォーム  >`}
           </button>
