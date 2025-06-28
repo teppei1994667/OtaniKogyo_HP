adc81bb34eec26c94acc86f75e21a0f15df98c25
笹田哲平 <teppei@sasadateppeinoMacBook-Pro.local>
2025-06-28 10:36:25 +0900

    app/page: metadata追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import { AboutUs } from "./share/topPageComponent/AboutUs";
 import { Contact } from "./share/topPageComponent/Contact";
 import { MainImage } from "./share/topPageComponent/MainImage";
@@ -5,6 +6,30 @@ import { News } from "./share/topPageComponent/News";
 import { OurService } from "./share/topPageComponent/OurService";
 import { OurWorks } from "./share/topPageComponent/OurWorks";
 import { SplashScreen } from "./share/topPageComponent/SplashScreen";
+
+export const metadata: Metadata = {
+  title: "株式会社大谷工業 | アスベスト除去の専門業者",
+  description:
+    "大谷工業はアスベスト除去、ダイオキシン対策に対応。安全・安心な工事を全国対応で提供します。",
+  robots: "index, follow",
+  keywords: ["アスベスト除去", "ダイオキシン工事", "解体工事"],
+  alternates: {
+    canonical: "https://example.com/",
+  },
+  openGraph: {
+    title: "株式会社大谷工業 | アスベスト除去の専門業者",
+    description: "安心・安全なアスベスト除去工事を全国対応で実施。",
+    url: "https://example.com/",
+    siteName: "株式会社大谷工業",
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "株式会社大谷工業 | アスベスト除去の専門業者",
+    description: "アスベストやダイオキシン類除去に特化した専門会社です。",
+  },
+};
 
 export default function Home() {
   return (
